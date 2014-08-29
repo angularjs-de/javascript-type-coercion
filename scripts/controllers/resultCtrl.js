d3166312891db77181720bf15a40232f167102ae
Sascha Brink <sascha.brink@gmail.com>
2014-08-29 15:07:55 +0200

    Add end result

--- scripts/controllers/resultCtrl.js
+++ scripts/controllers/resultCtrl.js
@@ -4,6 +4,7 @@ angular.module('typeCoercion')
   .controller('summaryCtrl', function ($scope, $location, resultHolderService) {
     $scope.questions = resultHolderService.getQuestions();
     $scope.results = resultHolderService.getResults();
+    $scope.correctAnswersCount = resultHolderService.getCorrectAnswersCount();
 
     if (!$scope.results.length) {
       $location.path('/');
--- scripts/services/resultHolderService.js
+++ scripts/services/resultHolderService.js
@@ -1,20 +1,29 @@
 angular.module('typeCoercion')
-  .factory('resultHolderService', function(questions) {
+  .factory('resultHolderService', function(questions, $filter) {
     'use strict';
     function shuffle(o) { //v1.0
       for (var j, x, i = o.length; i; j = Math.floor(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x);
       return o;
     };
     var results = [];
-    var tasks = shuffle(questions);
+    var questions = shuffle(questions);
+
+    function getCorrectAnswersCount() {
+      console.log(results);
+      return questions.filter(function(question, index){
+        console.log(index, $filter('eval')(question), results[index])
+        return $filter('eval')(question) === results[index];
+      }).length
+    }
 
     return {
-      getQuestions: function () {
-        return tasks;
+      getQuestions: function() {
+        return questions;
       },
-      getResults: function () {
+      getResults: function() {
         return results;
-      }
+      },
+      getCorrectAnswersCount: getCorrectAnswersCount
     };
 
   });
